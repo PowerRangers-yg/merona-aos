f170eaff385a3188d58a25cf197873eb1b43a5ed
GyuriShim <rabitskr@naver.com>
2023-03-08 20:23:43 +0900

    역할 선택 화면 추가(#1)

--- merona/app/src/main/java/com/example/allttaemerona/src/main/SelectActivity.kt
+++ merona/app/src/main/java/com/example/allttaemerona/src/main/SelectActivity.kt
@@ -3,10 +3,11 @@ package com.example.allttaemerona.src.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.allttaemerona.R
+import com.example.allttaemerona.config.BaseActivity
+import com.example.allttaemerona.databinding.ActivitySelectBinding
 
-class MainActivity : AppCompatActivity() {
+class SelectActivity : BaseActivity<ActivitySelectBinding>(ActivitySelectBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
