2a2ea181fbb3f2bcd0da5c7759e856614b4741fd
GyuriShim <rabitskr@naver.com>
2023-03-08 20:22:47 +0900

    닉네임 화면 추가(#1)

---
+++ merona/app/src/main/java/com/example/allttaemerona/src/signup/NicknameActivity.kt
@@ -0,0 +1,13 @@
+package com.example.allttaemerona.src.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.allttaemerona.R
+import com.example.allttaemerona.config.BaseActivity
+import com.example.allttaemerona.databinding.ActivityNicknameBinding
+
+class NicknameActivity : BaseActivity<ActivityNicknameBinding>(ActivityNicknameBinding::inflate){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
