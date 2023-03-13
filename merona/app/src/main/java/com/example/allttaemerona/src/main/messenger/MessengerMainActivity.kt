cc06a7f15c39a1f2614af11c4ad0e6ad25ddc58f
GyuriShim <rabitskr@naver.com>
2023-03-13 22:33:14 +0900

    전달자 bottom nav

--- merona/app/src/main/java/com/example/allttaemerona/src/main/messenger/MessengerMainActivity.kt
+++ merona/app/src/main/java/com/example/allttaemerona/src/main/messenger/MessengerMainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.allttaemerona.src.main.messenger
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.allttaemerona.R
+import com.example.allttaemerona.config.BaseActivity
+import com.example.allttaemerona.databinding.ActivityMessengerMainBinding
+import com.example.allttaemerona.src.main.chat.ChatListFragment
+import com.example.allttaemerona.src.main.messenger.check.DeliveryCheckFragment
 
-class MessengerMainActivity : AppCompatActivity() {
+class MessengerMainActivity : BaseActivity<ActivityMessengerMainBinding>(ActivityMessengerMainBinding::inflate){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_messenger_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.messengerFrm.id, DeliveryCheckFragment())
+            .commitAllowingStateLoss()
+
+        binding.navBottomMes.run {
+            setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.menu_check -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.messengerFrm.id, DeliveryCheckFragment())
+                            .commitAllowingStateLoss()
+                    }
+                    R.id.menu_messenger_chat -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.messengerFrm.id, ChatListFragment())
+                            .commitAllowingStateLoss()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.menu_check
+        }
     }
 }
