d7f6b0baa10678a302dd28acbe0bb1715be37ac7
GyuriShim <rabitskr@naver.com>
2023-03-15 21:15:16 +0900

    설정 화면(#10)

---
+++ merona/app/src/main/java/com/example/allttaemerona/src/setting/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.allttaemerona.src.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.allttaemerona.config.BaseActivity
+import com.example.allttaemerona.databinding.ActivitySettingBinding
+
+class SettingActivity : BaseActivity<ActivitySettingBinding>(ActivitySettingBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
