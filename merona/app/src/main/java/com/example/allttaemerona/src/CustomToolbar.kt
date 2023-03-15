08efe3a4fd103c9c58e95ee022447ffe5593294e
GyuriShim <rabitskr@naver.com>
2023-03-15 21:09:26 +0900

    툴바 attr 수정

--- merona/app/src/main/java/com/example/allttaemerona/src/CustomToolbar.kt
+++ merona/app/src/main/java/com/example/allttaemerona/src/CustomToolbar.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ContentInfoCompat.Flags
 import com.example.allttaemerona.R
 
 class CustomToolbar(context: Context, attrs:AttributeSet) : ConstraintLayout(context,attrs) {
 
     private var showBackButton : Boolean = false
+    private var showButtons : Boolean = true
     private lateinit var title: String
     init {
         initAttrs(attrs)
@@ -25,6 +27,7 @@ class CustomToolbar(context: Context, attrs:AttributeSet) : ConstraintLayout(con
         ).apply {
             try {
                 showBackButton = getBoolean(R.styleable.CustomToolbar_showBackButton, false)
+                showButtons = getBoolean(R.styleable.CustomToolbar_showButtons, true)
                 title = getString(R.styleable.CustomToolbar_title) ?: ""
             } finally {
                 recycle()
@@ -37,9 +40,17 @@ class CustomToolbar(context: Context, attrs:AttributeSet) : ConstraintLayout(con
 
         val titleTextView = findViewById<TextView>(R.id.tv_title)
         val backButton = findViewById<ImageButton>(R.id.ib_back)
+        val switchButton = findViewById<ImageButton>(R.id.ib_switch)
+        val notificationButton = findViewById<ImageButton>(R.id.ib_notification)
+        val settingButton = findViewById<ImageButton>(R.id.ib_setting)
 
         titleTextView.text = title
         if(!showBackButton) backButton.visibility = View.GONE
+        if(!showButtons) {
+            switchButton.visibility = View.GONE
+            notificationButton.visibility = View.GONE
+            settingButton.visibility = View.GONE
+        }
     }
 
     fun setOnBackListner(listner: OnBackListener){
