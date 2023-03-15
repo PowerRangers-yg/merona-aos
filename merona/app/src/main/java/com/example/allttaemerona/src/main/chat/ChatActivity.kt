fa9653efaeef58e26a5e5c396c08306fb9e3139b
GyuriShim <rabitskr@naver.com>
2023-03-15 21:13:22 +0900

    채팅 화면(#6)

---
+++ merona/app/src/main/java/com/example/allttaemerona/src/main/chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.allttaemerona.src.main.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.allttaemerona.config.BaseActivity
+import com.example.allttaemerona.databinding.ActivityChatBinding
+
+class ChatActivity : BaseActivity<ActivityChatBinding>(ActivityChatBinding::inflate){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
