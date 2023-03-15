30a44b05bc210d7a6a101c53ab6b61d154b4a9b0
GyuriShim <rabitskr@naver.com>
2023-03-15 21:16:05 +0900

    알림 화면(#9)

---
+++ merona/app/src/main/java/com/example/allttaemerona/src/notification/NotificationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.allttaemerona.src.notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.allttaemerona.config.BaseActivity
+import com.example.allttaemerona.databinding.ActivityNotificationBinding
+
+class NotificationActivity : BaseActivity<ActivityNotificationBinding>(ActivityNotificationBinding::inflate){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
