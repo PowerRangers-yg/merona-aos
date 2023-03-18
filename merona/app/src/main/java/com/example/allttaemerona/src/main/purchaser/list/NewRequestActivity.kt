02f9aaea5cd27acde4be1ff09f8ee93d47309b8b
GyuriShim <rabitskr@naver.com>
2023-03-18 23:28:47 +0900

    디자인 수정

--- merona/app/src/main/java/com/example/allttaemerona/src/main/purchaser/list/NewRequestActivity.kt
+++ merona/app/src/main/java/com/example/allttaemerona/src/main/purchaser/list/NewRequestActivity.kt
@@ -1,4 +1,11 @@
 package com.example.allttaemerona.src.main.purchaser.list
 
-class NewRequestActivity {
+import android.os.Bundle
+import com.example.allttaemerona.config.BaseActivity
+import com.example.allttaemerona.databinding.ActivityNewRequestBinding
+
+class NewRequestActivity: BaseActivity<ActivityNewRequestBinding>(ActivityNewRequestBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
