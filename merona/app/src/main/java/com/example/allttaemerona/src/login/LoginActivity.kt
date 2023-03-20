a13b77d69b77222dd02dd54509e5a73b8c741fb1
GyuriShim <rabitskr@naver.com>
2023-03-20 23:07:55 +0900

    로그인 화면 연결(#11)

--- merona/app/src/main/java/com/example/allttaemerona/src/login/LoginActivity.kt
+++ merona/app/src/main/java/com/example/allttaemerona/src/login/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.example.allttaemerona.src.login
 
+import android.content.Intent
 import android.os.Bundle
 import com.example.allttaemerona.config.BaseActivity
 import com.example.allttaemerona.databinding.ActivityLoginBinding
+import com.example.allttaemerona.src.main.SelectActivity
+import com.example.allttaemerona.src.signup.SignupActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.btnSignup.setOnClickListener {
+            var intent = Intent(this, SignupActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            var intent = Intent(this, SelectActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
