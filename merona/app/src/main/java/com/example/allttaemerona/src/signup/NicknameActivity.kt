1fb04849d9243e4f691ccbca701fbee70bb6d1a9
GyuriShim <rabitskr@naver.com>
2023-03-20 23:13:33 +0900

    회원가입 화면 연결(#12)

--- merona/app/src/main/java/com/example/allttaemerona/src/signup/NicknameActivity.kt
+++ merona/app/src/main/java/com/example/allttaemerona/src/signup/NicknameActivity.kt
@@ -1,13 +1,25 @@
 package com.example.allttaemerona.src.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.allttaemerona.R
 import com.example.allttaemerona.config.BaseActivity
 import com.example.allttaemerona.databinding.ActivityNicknameBinding
+import com.example.allttaemerona.src.login.LoginActivity
 
 class NicknameActivity : BaseActivity<ActivityNicknameBinding>(ActivityNicknameBinding::inflate){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+
+        binding.btnJoin.setOnClickListener {
+            var intent = Intent(this, LoginActivity::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        }
     }
 }
--- merona/app/src/main/java/com/example/allttaemerona/src/signup/SignupActivity.kt
+++ merona/app/src/main/java/com/example/allttaemerona/src/signup/SignupActivity.kt
@@ -1,5 +1,6 @@
 package com.example.allttaemerona.src.signup
 
+import android.content.Intent
 import android.os.Bundle
 import com.example.allttaemerona.config.BaseActivity
 import com.example.allttaemerona.databinding.ActivitySignupBinding
@@ -7,5 +8,14 @@ import com.example.allttaemerona.databinding.ActivitySignupBinding
 class SignupActivity : BaseActivity<ActivitySignupBinding>(ActivitySignupBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.btnCancel.setOnClickListener {
+            finish()
+        }
+
+        binding.btnNext.setOnClickListener {
+            var intent = Intent(this, NicknameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
