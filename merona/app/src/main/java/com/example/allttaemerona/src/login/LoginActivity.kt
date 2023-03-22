da9158c805a2360f5bdd88d4f5cc5f58559dca11
GyuriShim <rabitskr@naver.com>
2023-03-22 15:29:38 +0900

    이메일 로그인 구현(#11)

--- merona/app/src/main/java/com/example/allttaemerona/src/login/LoginActivity.kt
+++ merona/app/src/main/java/com/example/allttaemerona/src/login/LoginActivity.kt
@@ -2,6 +2,10 @@ package com.example.allttaemerona.src.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import com.example.allttaemerona.config.BaseActivity
 import com.example.allttaemerona.databinding.ActivityLoginBinding
 import com.example.allttaemerona.src.main.SelectActivity
@@ -11,16 +15,37 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::i
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.btnSignup.setOnClickListener {
+        val idEdit: EditText = binding.etId
+        val pwEdit: EditText = binding.etPw
+        val btnSignup: Button = binding.btnSignup
+        val btnLogin: Button = binding.btnLogin
+        var id: String = ""
+        var pw: String = ""
+
+
+        //회원가입 버튼
+        btnSignup.setOnClickListener {
             var intent = Intent(this, SignupActivity::class.java)
 
             startActivity(intent)
         }
 
-        binding.btnLogin.setOnClickListener {
-            var intent = Intent(this, SelectActivity::class.java)
-            startActivity(intent)
-            finish()
+        //로그인 버튼
+        btnLogin.setOnClickListener {
+            id = idEdit.text.toString()
+            pw = pwEdit.text.toString()
+
+            if (id.isEmpty() || pw.isEmpty()){
+                showCustomToast("아이디와 비밀번호를 입력해주세요")
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(id).matches()){
+                showCustomToast("유효하지 않은 이메일 주소입니다.")
+            } else {
+                var intent = Intent(this, SelectActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            //else if (아이디 비밀번호 일치 X)
+
         }
     }
 }
