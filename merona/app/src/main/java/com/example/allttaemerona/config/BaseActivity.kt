8cc73d3e2544bad941f5d7bad8dd9b59b4ad3ca9
GyuriShim <rabitskr@naver.com>
2023-04-05 18:29:36 +0900

    사진 등록 기능 추가(#15)

--- merona/app/src/main/java/com/example/allttaemerona/config/BaseActivity.kt
+++ merona/app/src/main/java/com/example/allttaemerona/config/BaseActivity.kt
@@ -1,6 +1,9 @@
 package com.example.allttaemerona.config
 
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,5 +21,22 @@ abstract class BaseActivity <B: ViewBinding>(private val inflate: (LayoutInflate
 
     fun showCustomToast(message: String){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    //이미지 실제 경로 반환
+    fun getRealPathFromURI(uri: Uri): String {
+        val buildName = Build.MANUFACTURER
+        if (buildName.equals("Xiaomi")){
+            return uri.path!!
+        }
+        var columnIndex = 0
+        var proj = arrayOf(MediaStore.Images.Media.DATA)
+        var cursor = contentResolver.query(uri, proj, null, null, null)
+        if (cursor!!.moveToFirst()){
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+        val result = cursor.getString(columnIndex)
+        cursor.close()
+        return result
     }
 }
--- merona/app/src/main/java/com/example/allttaemerona/src/main/purchaser/list/NewRequestActivity.kt
+++ merona/app/src/main/java/com/example/allttaemerona/src/main/purchaser/list/NewRequestActivity.kt
@@ -1,12 +1,46 @@
 package com.example.allttaemerona.src.main.purchaser.list
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.allttaemerona.config.BaseActivity
 import com.example.allttaemerona.databinding.ActivityNewRequestBinding
 import com.example.allttaemerona.src.CustomToolbar
+import java.io.File
 
 class NewRequestActivity: BaseActivity<ActivityNewRequestBinding>(ActivityNewRequestBinding::inflate) {
+    lateinit var imageFile: File
+
+    companion object{
+        //갤러리 권한 요청
+        const val REQ_GALLERY = 1
+    }
+    //이미지를 결과값으로 받는 변수
+    private val imageResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){ result ->
+       if (result.resultCode == RESULT_OK){
+            val imageUri = result.data?.data
+           imageUri?.let {
+               imageFile = File(getRealPathFromURI(it))
+
+               Glide.with(this)
+                   .load(imageUri)
+                   .fitCenter()
+                   .apply(RequestOptions().override(500,500))
+                   .into(binding.ivImage)
+           }
+       }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,7 +50,11 @@ class NewRequestActivity: BaseActivity<ActivityNewRequestBinding>(ActivityNewReq
 
         val editTime = binding.tvTimeSetting
         val editPay = binding.tvPaySetting
+        var imageSelect = binding.btnPhoto
+        var btnCancel = binding.btnCancel
+        var btnPost = binding.btnPost
 
+        //number picker 시간
         editTime.setOnClickListener {
             var initNum = 0
             if (editTime.text != null ){
@@ -30,6 +68,7 @@ class NewRequestActivity: BaseActivity<ActivityNewRequestBinding>(ActivityNewReq
             dialog.show(supportFragmentManager, "time" )
         }
 
+        //number picker 가격
         editPay.setOnClickListener{
             var initNum = 0
             if (editPay.text != null ){
@@ -42,12 +81,46 @@ class NewRequestActivity: BaseActivity<ActivityNewRequestBinding>(ActivityNewReq
             dialog.isCancelable = false
             dialog.show(supportFragmentManager, "pay" )
         }
+
+        //사진 등록 버튼
+        imageSelect.setOnClickListener {
+            selectGallery()
+        }
+
+        //요청 작성 취소 버튼
+        btnCancel.setOnClickListener {
+            finish()
+        }
+
+        //요청 등록 버튼 (추후 기능 추가)
+        btnPost.setOnClickListener {
+            finish()
+        }
     }
 
     //toolbar 뒤로가기 버튼
     inner class OnBackListener : CustomToolbar.OnBackListener {
         override fun onClick(view: View) {
             finish()
+        }
+    }
+
+    //갤러리 호출
+    private fun selectGallery(){
+        val writePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val readPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if (writePermission == PackageManager.PERMISSION_DENIED ||
+                readPermission == PackageManager.PERMISSION_DENIED) {
+            //권한 요청
+            ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), REQ_GALLERY)
+        } else {
+            //권한이 있는 경우 갤러리 실행
+            val intent = Intent(Intent.ACTION_PICK)
+            //intent의 data와 type을 동시에 설정하는 매서드
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+            imageResult.launch(intent)
         }
     }
 }
